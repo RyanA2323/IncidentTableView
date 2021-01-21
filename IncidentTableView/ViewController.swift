cbf4f49f8a1c9014299fe1b8befd1ba698064d5b
Ryan <ryan.atkinson21@d155.org>
2021-01-21 17:12:18 -0600

    Initial Commit

---
+++ IncidentTableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IncidentTableView
+//
+//  Created by Tiger Coder on 1/21/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
