f78a5bf7b6bcbffcc757e66d3228a6f1397a05e2
Ryan <ryan.atkinson21@d155.org>
2021-01-26 08:26:55 -0600

    Time and info button

--- IncidentTableView/CustomCell.swift
+++ IncidentTableView/CustomCell.swift
@@ -11,9 +11,12 @@ class CustomCell: UITableViewCell {
 
     @IBOutlet weak var imageIcon: UIImageView!
     @IBOutlet weak var incidentLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var infoButton: UIButton!
     
     func configure(pic: UIImage, incident: String) {
         incidentLabel.text = incident
         imageIcon.image = pic
+        //time not included yet
     }
 }
---
+++ IncidentTableView/ViewController3.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController3.swift
+//  IncidentTableView
+//
+//  Created by Tiger Coder on 1/26/21.
+//
+
+import UIKit
+
+class ViewController3: UIViewController {
+    @IBOutlet weak var textViewOutlet: UITextView!
+    @IBOutlet weak var refreshButtonOutlet: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
