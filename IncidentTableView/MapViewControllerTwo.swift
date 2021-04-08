12e851e646fe38f17c503cbcd3893bbe7d064b43
Colin Hehn <colin45h@yahoo.com>
2021-04-08 11:19:35 -0500

    Added Scaffolding for Aiden

---
+++ IncidentTableView/MapViewControllerTwo.swift
@@ -0,0 +1,29 @@
+//
+//  MapViewControllerTwo.swift
+//  IncidentTableView
+//
+//  Created by Tiger Coder on 4/8/21.
+//
+
+import UIKit
+
+class MapViewControllerTwo: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
