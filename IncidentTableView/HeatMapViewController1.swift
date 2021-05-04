624112390737d441102ee0f86f767b853e6ac290
Aiden <meaneea@gmail.com>
2021-05-04 09:38:23 -0500

    Make heat map display some dummy data. Heat map has constraints now.

--- IncidentTableView/HeatMapViewController1.swift
+++ IncidentTableView/HeatMapViewController1.swift
@@ -10,19 +10,24 @@ import UIKit
 class HeatMapViewController1: UIViewController {
     
     @IBOutlet weak var incidentLabel: UILabel!
+    @IBOutlet weak var incidentLocationView: IncidentLocationView!
     var currentIncidentKey: String = "DemoIncident"
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        incidentLocationView.setFloorImages([UIImage(named: "Floor1")!, UIImage(named: "floor2")!])
         // Do any additional setup after loading the view.
     }
     
-    override func viewDidAppear(_ animated: Bool) {
+    override func viewWillAppear(_ animated: Bool) {
         if let sesh = defaults.object(forKey: "currentIncidentKey") as? String {
             currentIncidentKey = sesh
             print(currentIncidentKey)
             incidentLabel.text = "For Incident: \(defaults.value(forKey: "typeIncHeatMap")!)"
+            
+            // Changing the floor parameter between 1 and 2 will automatically switch the image.
+            let DUMMY_LOCATION = Location(pos: CGPoint(x: 0.3, y: 0.7), floor: 2);
+            incidentLocationView.setLocation(DUMMY_LOCATION);
         }
     }
     
