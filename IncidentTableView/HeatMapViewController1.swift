835a6ad5838adce8f52212ba6b83eb36a5b90c9a
Colin Hehn <colin45h@yahoo.com>
2021-05-09 22:41:03 -0500

    Heatmap display scaffolding

--- IncidentTableView/HeatMapViewController1.swift
+++ IncidentTableView/HeatMapViewController1.swift
@@ -23,12 +23,49 @@ class HeatMapViewController1: UIViewController {
         if let sesh = defaults.object(forKey: "currentIncidentKey") as? String {
             currentIncidentKey = sesh
             print(currentIncidentKey)
-            incidentLabel.text = "For Incident: \(defaults.value(forKey: "typeIncHeatMap")!)"
             
-            // Changing the floor parameter between 1 and 2 will automatically switch the image.
-            let DUMMY_LOCATION = Location(pos: CGPoint(x: 0.3, y: 0.7), floor: 1);
-            incidentLocationView.setLocation(DUMMY_LOCATION);
+            
+            incidentLabel.text = "For Incident: \(defaults.value(forKey: "typeIncHeatMap") ?? "Defaults Error")"
+            
+            db.collection("incidents").document(currentIncidentKey).collection("subInformation").getDocuments { (querySnapshot, err) in
+                if let err = err {
+                    print("Error getting documents: \(err)")
+                } else {
+                    
+                    var pointArray: [CGPoint] = []
+                    
+                    for document in querySnapshot!.documents {
+                        // print("\(document.documentID) => \(document.data())")
+                        
+                        let incidentMade = Incident(inc: .other)
+                        incidentMade.key = document.documentID
+                        
+                        // Temporary Debug Statement: Prints each map coordinate in the console.s
+                        // print("Incident (\(document.documentID) - PointX: \(incidentMade.pointx ?? 0), PointY: \(incidentMade.pointy ?? 0)")
+                        
+                        for dt in document.data() {
+                            if dt.key == "pointx" {
+                                incidentMade.pointx = dt.value as? CGFloat
+                            }
+                            if dt.key == "pointy" {
+                                incidentMade.pointy = dt.value as? CGFloat
+                            }
+                        }
+                        
+                        let pointAdd = CGPoint(x: incidentMade.pointx ?? 0, y: incidentMade.pointy ?? 0)
+                        pointArray.append(pointAdd)
+                    }
+                    
+                    for point in pointArray {
+                        let locationDisplay = Location(pos: point, floor: 1)
+                        self.incidentLocationView.setLocation(locationDisplay)
+                    }
+                    
+                    // Changing the floor parameter between 1 and 2 will automatically switch the image.
+                    // let DUMMY_LOCATION = Location(pos: CGPoint(x: 0.3, y: 0.7), floor: 2);
+                    // self.incidentLocationView.setLocation(DUMMY_LOCATION);
+                }
+            }
         }
     }
-    
 }
--- IncidentTableView/infoScreen.swift
+++ IncidentTableView/infoScreen.swift
@@ -20,7 +20,7 @@ class infoScreen: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        textOutlet.text = "Situational Examples: \n \n CODE RED \n -Emergency standard \n -Active shooter \n -Unusual suspect \n -Carrier of a weapon \n \n FIGHT \n -Fight about to occur or possible fight risk \n -Fight in progress \n \n MEDICAL \n -Injury \n -Student or teacher fainted \n \n OTHER \n -Unusal person in or near building \n -Gas smell \n -Building maintence"
+        textOutlet.text = "Situational Examples: \n \n CODE RED \n - Emergency standard \n - Active shooter \n - Unusual suspect \n - Carrier of a weapon \n \n FIGHT \n - Fight about to occur or possible fight risk \n - Fight in progress \n \n MEDICAL \n - Injury \n -Student or teacher fainted \n \n OTHER \n - Unusal person in or near building \n - Gas smell \n - Building maintence"
         
         
         
